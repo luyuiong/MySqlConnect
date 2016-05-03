ea3def6613148755280b42a00eb5d81b74a2658f
LuYulong <195586535@qq.com>
2016-05-03 17:33:39 +0800

    1

---
+++ MySqlConnect/MySqlConnect/DbConnectionMySql.cs
@@ -0,0 +1,59 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlConnect
+{
+    /// <summary>
+    /// MySql数据库连接类
+    /// </summary>
+    public class DbConnectionMySql
+    {
+        /// <summary>
+        /// MySql数据库连接
+        /// </summary>
+        private MySqlConnection m_mySqlCon;
+
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        /// <param name="conStr">MySql数据库连接字符串</param>
+        public DbConnectionMySql(string conStr)
+        {
+            m_mySqlCon = new MySqlConnection(conStr);
+        }
+
+        /// <summary>
+        /// 执行MySqlCommand
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        public void getmysqlcom(string M_str_sqlstr)
+        {
+            m_mySqlCon.Open();
+            MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, m_mySqlCon);
+            mysqlcom.ExecuteNonQuery();
+            mysqlcom.Dispose();
+            m_mySqlCon.Close();
+            m_mySqlCon.Dispose();
+        }
+
+        /// <summary>
+        /// 创建一个MySqlDataReader对象
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        /// <returns>返回MySqlDataReader对象</returns>
+        public MySqlDataReader getmysqlread(string M_str_sqlstr)
+        {
+            MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, m_mySqlCon);
+            m_mySqlCon.Open();
+            MySqlDataReader mysqlread = mysqlcom.ExecuteReader(CommandBehavior.CloseConnection);
+            mysqlcom.Dispose();
+            m_mySqlCon.Close();
+            return mysqlread;
+        }
+    }
+}
---
+++ MySqlConnect/MySqlConnect/Program.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string M_str_sqlcon = "server=localhost;user id=root;password=1;database=testMySql"; //根据自己的设置
+            DbConnectionMySql dbMySql = new DbConnectionMySql(M_str_sqlcon);
+            dbMySql.getmysqlcom("insert into runoob_tbl values(3, 'Learn C++', 'luyulong', NOW())");
+
+            MySqlDataReader dataReader = dbMySql.getmysqlread("select * from runoob_tbl");
+
+            // 判断数据是否读到尾. 
+            while (dataReader.Read()) 
+            { 
+                Console.WriteLine(String.Format("{0}, {1}",
+                dataReader[0], dataReader[1])); 
+            } 
+            // 一定要关闭 reader 对象. 
+            dataReader.Close(); 
+        }
+
+
+    }
+
+
+}
